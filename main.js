8f4a9a0016e92a8d7a326f115bceeaeaa422ca87
Francisco Nuñez <78834731+Panchiusly@users.noreply.github.com>
2023-12-25 00:36:27 +0100

    Fix issue by allowing insensitive case input
    Add variable to convert input to uppercase to prevent only sensitive case answers

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-let userSelection = prompt ("What's your winner hand today?")
+let input = prompt ("What's your winner hand today?")
+
+let userSelection = input.toUpperCase ();
 console.log(userSelection)
 
 let computerSelection = getComputerChoice ();
 console.log(computerSelection)
 
 function getComputerChoice () {
-    let hands = ["Rock", "Paper", "Scissors"]
+    let hands = ["ROCK", "PAPER", "SCISSORS"]
     let computerSelection = hands [Math.floor(Math.random() * hands.length)];
     // I don't really understand why this Math.floor and Math.random works. But it does.
     return computerSelection;
@@ -15,21 +17,24 @@ let roundWinner = getRoundWinner ();
 console.log(roundWinner)
 
 function getRoundWinner () {
-    if (userSelection == "Rock" && computerSelection == "Scissors") {
+    if (userSelection == "ROCK" && computerSelection == "SCISSORS") {
         alert ("You fricking win! Rock beats Scissors")
-    } else if (userSelection == "Paper" && computerSelection == "Rock") {
+    } else if (userSelection == "PAPER" && computerSelection == "ROCK") {
         alert ("You fricking win! Paper beats Rock")
-    } else if (userSelection == "Scissors" && computerSelection == "Paper") {
+    } else if (userSelection == "SCISSORS" && computerSelection == "PAPER") {
         alert ("You fricking win! Scissors beat Paper")
-    } else if (userSelection == "Scissors" && computerSelection == "Rock") {
+    } else if (userSelection == "SCISSORS" && computerSelection == "ROCK") {
         alert ("You loser! Rock beats Scissors")
-    } else if (userSelection == "Paper" && computerSelection == "Scissors") {
+    } else if (userSelection == "PAPER" && computerSelection == "SCISSORS") {
         alert ("You loser! Scissors beat Paper")
-    } else if (userSelection == "Rock" && computerSelection == "Paper") {
+    } else if (userSelection == "ROCK" && computerSelection == "PAPER") {
         alert ("You loser! Paper beats Rock")
-    } else {
+    } else if (userSelection == computerSelection){
         alert ("Tie!")
         //I gotta check this cuz, even if the input is other like "ROck" it will take it as a tie.
+        //Fixed by converting the answer to uppercase.
+    } else {
+        alert ("Error")
     }
 
 }

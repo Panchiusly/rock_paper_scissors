e44d17e076a18e04b6e106131dc273730b7d3fe3
Francisco Nuñez <78834731+Panchiusly@users.noreply.github.com>
2023-12-25 12:27:59 +0100

    Add new variable and function
    New function "game" allows code to run 5 times by variable "playRound"

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
-let input = prompt ("What's your winner hand today?")
 
-let userSelection = input.toUpperCase ();
-console.log(userSelection)
+let playRound = (game (), game (), game (), game (), game ());
+// Ok, only in this way I can call the function 5 times. 
 
-let computerSelection = getComputerChoice ();
-console.log(computerSelection)
+function game () {
+    //I think everything should be inside of this function.   
+    let input = prompt ("What's your winner hand today?")
+    let userSelection = input.toUpperCase ();
+    let computerSelection = getComputerChoice ();
 
-function getComputerChoice () {
-    let hands = ["ROCK", "PAPER", "SCISSORS"]
-    let computerSelection = hands [Math.floor(Math.random() * hands.length)];
-    // I don't really understand why this Math.floor and Math.random works. But it does.
-    return computerSelection;
+    function getComputerChoice () {
+        let hands = ["ROCK", "PAPER", "SCISSORS"]
+        let computerSelection = hands [Math.floor(Math.random() * hands.length)];
+        // I don't really understand why this Math.floor and Math.random works. But it does.
+        return computerSelection;
 }
 
-let roundWinner = getRoundWinner ();
-console.log(roundWinner)
+    let roundWinner = getRoundWinner ();
 
-function getRoundWinner () {
-    if (userSelection == "ROCK" && computerSelection == "SCISSORS") {
-        alert ("You fricking win! Rock beats Scissors")
-    } else if (userSelection == "PAPER" && computerSelection == "ROCK") {
-        alert ("You fricking win! Paper beats Rock")
-    } else if (userSelection == "SCISSORS" && computerSelection == "PAPER") {
-        alert ("You fricking win! Scissors beat Paper")
-    } else if (userSelection == "SCISSORS" && computerSelection == "ROCK") {
-        alert ("You loser! Rock beats Scissors")
-    } else if (userSelection == "PAPER" && computerSelection == "SCISSORS") {
-        alert ("You loser! Scissors beat Paper")
-    } else if (userSelection == "ROCK" && computerSelection == "PAPER") {
-        alert ("You loser! Paper beats Rock")
-    } else if (userSelection == computerSelection){
-        alert ("Tie!")
-        //I gotta check this cuz, even if the input is other like "ROck" it will take it as a tie.
-        //Fixed by converting the answer to uppercase.
-    } else {
-        alert ("Error")
-    }
-
+    function getRoundWinner () {
+        if (userSelection == "ROCK" && computerSelection == "SCISSORS") {
+            alert ("You fricking win! Rock beats Scissors")
+        } else if (userSelection == "PAPER" && computerSelection == "ROCK") {
+            alert ("You fricking win! Paper beats Rock")
+        } else if (userSelection == "SCISSORS" && computerSelection == "PAPER") {
+            alert ("You fricking win! Scissors beat Paper")
+        } else if (userSelection == "SCISSORS" && computerSelection == "ROCK") {
+            alert ("You loser! Rock beats Scissors")
+        } else if (userSelection == "PAPER" && computerSelection == "SCISSORS") {
+            alert ("You loser! Scissors beat Paper")
+        } else if (userSelection == "ROCK" && computerSelection == "PAPER") {
+            alert ("You loser! Paper beats Rock")
+        } else if (userSelection == computerSelection){
+            alert ("Tie!")
+            //I gotta check this cuz, even if the input is other like "ROck" it will take it as a tie.
+            //Fixed by converting the answer to uppercase.
+        } else {
+            alert ("Error")
+        }
 }
-
-
+}
 
 
 
